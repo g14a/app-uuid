b1f4030e8a785c30a2276cf4b5099919e817d28c
Gowtham Munukutla <gowtham.m81197@gmail.com>
2020-03-04 01:09:14 +0530

    debugging gone wrong.
    Signed-off-by: Gowtham Munukutla <gowtham.m81197@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_uuid/models/models.dart';
 import 'package:flutter_uuid/page_two.dart';
+import 'package:http/http.dart' as http;
 
 void main () => runApp(MyApp());
 
@@ -12,26 +16,62 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: new HomePage(),
+      home: new MyHomePage(title: "Users"),
       routes: <String, WidgetBuilder>{
         "/a": (BuildContext context) => new NewPage("New Page"),
       });
   }
 }
 
-class HomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key:key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+    this._getUsers();
+  }
+
+  Future<List<PersonModel>> _getUsers() async {
+    final String url = "https://jsonplaceholder.typicode.com/todos/1";
+    var data = await http.get(url);
+
+    var jsonData = json.decode(data.body);
+    debugPrint(data.body);
+
+    List<PersonModel> users = [];
+
+    for (var p in jsonData) {
+      print("came into the loop");
+      PersonModel person = PersonModel(p['userId'],p['id'],p['title'],p['body']);
+      print("the value is ${person.title}");
+      users.add(person);
+    }
+
+    return users;
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("UUID"),
+        title: new Text(widget.title),
         elevation: defaultTargetPlatform == TargetPlatform.android ? 5.0 : 0.0,
       ),
       drawer: new Drawer(
         child: new ListView(
           children: <Widget> [
             new UserAccountsDrawerHeader(
-              accountName: new Text("Gowtham Munukutla"), 
+              accountName: new Text("Gowtham Munukutla"),
               accountEmail: new Text("gowtham@atlan.com"),
               currentAccountPicture: new CircleAvatar(
                 backgroundColor: Colors.black,
@@ -57,9 +97,28 @@ class HomePage extends StatelessWidget {
         ),
       ),
       body: new Container(
-        child: new Center(
-          child: new Text("Home"),
-        ),
+        child: new FutureBuilder(
+            future: _getUsers(),
+            builder: (BuildContext context, AsyncSnapshot<List<PersonModel>> snapshot) {
+              if (snapshot.data == null) {
+                print("null data");
+                return Container(
+                  child: Center(
+                    child: Text("Loading..."),
+                  ),
+                );
+              } else {
+                return ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                        title: Text(snapshot.data[index].userId),
+                      );
+                    }
+                );
+              }
+            }
+        )
       ),
     );
   }
---
+++ lib/models/models.dart
@@ -0,0 +1,8 @@
+class PersonModel {
+  final String userId;
+  final String id;
+  final String title;
+  final bool completed;
+
+  PersonModel(this.userId, this.id, this.title, this.completed);
+}
